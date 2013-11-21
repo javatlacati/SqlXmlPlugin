c43ecd9f6a1959bb6074041846d1a003eefb1342
daniel <daniel@192.168.2.204>
2013-11-21 21:51:05 +0100

    getting to connection

--- src/main/java/cz/syntea/nb/sqlxml/plugin/actions/RunSqlXmlAction.java
+++ src/main/java/cz/syntea/nb/sqlxml/plugin/actions/RunSqlXmlAction.java
@@ -26,11 +26,20 @@ package cz.syntea.nb.sqlxml.plugin.actions;
 import cz.syntea.nb.sqlxml.plugin.output.XDCMOutputTopComponent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+import org.netbeans.api.db.explorer.DatabaseConnection;
+import org.netbeans.modules.db.api.sql.execute.SQLExecuteCookie;
+import org.netbeans.modules.db.sql.loader.SQLEditorSupport;
 import org.openide.cookies.EditorCookie;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.Exceptions;
+import org.openide.util.Mutex.Action;
 import org.openide.util.NbBundle.Messages;
 
 @ActionID(
@@ -56,9 +65,25 @@ public final class RunSqlXmlAction implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent ev) {
+        //FileUtil.getConfigObject("path/to/print/action/in/layer.xml", Action.class).actionPerformed(ev);
+        // SQLEditorSupport sQLEditorSupport = (SQLEditorSupport)context;
+        //SQLExecuteCookie sQLEditorSupport = (SQLExecuteCookie)context;
+        Object invoked;
+        try {
+            invoked = context.getClass().getDeclaredMethod("getDatabaseConnection").invoke((Object)context);
+            if(invoked==null)return;
+            DatabaseConnection databaseConnection = (DatabaseConnection) invoked;
+        
+            JOptionPane.showMessageDialog(null, databaseConnection.getJDBCConnection());
+        } catch (Exception ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        
+        
         XDCMOutputTopComponent out;
         out = XDCMOutputTopComponent.getDefault();
-        out.printXML("<a ddd=\"sss\"/>");
+   //     out.printXML("<a ddd=\""+sQLEditorSupport.getDatabaseConnection().getDisplayName()+"\"/>");
+        out.printXML("<a ddd=\"dfgdfg\"/>");
         out.open();
         out.requestActive();
     }
